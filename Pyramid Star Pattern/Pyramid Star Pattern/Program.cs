e2e7f4504f4e3ae460fc7d0abf2c9593017f657f
mohammad <mo.se.12369@gmail.com>
2023-02-01 09:47:42 +0330

    Pyramid Star Pattern

---
+++ Pyramid Star Pattern/Pyramid Star Pattern/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pyramid_Star_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number : ");
+            String s = Console.ReadLine();
+            int a = Convert.ToInt32(s);
+
+            for (int i = 1; i <= a; i++)
+            {
+                for (int j = 1; j <= a - i; j++) Console.Write(" ");
+
+                for (int j = 1; j <= 2 * i - 1; j++) Console.Write("*");
+
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
